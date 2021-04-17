34fe06984504d827b46bcd2d00298f33a18e2953
Máté Steffler <mate.steffler@stud.uni-corvinus.hu>
2021-04-17 13:11:03 +0100

    8gyak

---
+++ HajosTeszt/HajosTeszt/Controllers/TesztController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HajosTeszt.Controllers
+{
+    [ApiController]
+    public class TesztController : Controller
+    {
+        [HttpGet]
+        [Route("corvinus/nagybetus/{szoveg}")]
+        public IActionResult M2(string szoveg)
+        {
+            return new ContentResult
+            {
+                ContentType = System.Net.Mime.MediaTypeNames.Text.Plain, //"text/plain"
+                Content = szoveg.ToUpper()
+            };
+        }
+    }
+}
--- HajosTeszt/HajosTeszt/Startup.cs
+++ HajosTeszt/HajosTeszt/Startup.cs
@@ -17,6 +17,7 @@ namespace HajosTeszt
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,7 +37,12 @@ namespace HajosTeszt
 
             app.UseRouting();
 
-            
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+
             //app.UseEndpoints(endpoints =>
             //{
             //   endpoints.MapGet("/", async context =>
@@ -44,7 +50,7 @@ namespace HajosTeszt
             //       await context.Response.WriteAsync("Hello World!");
             //   });
             //});
-            
+
         }
     }
 }
